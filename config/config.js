efe891f5d7f9a6fcfa00cc42b13dbae476d0cb2e
psihozykt <psihozykt@gmail.com>
2022-06-22 21:27:19 +0900

    trying to resolve db connection problem

--- config/config.js
+++ config/config.js
@@ -4,11 +4,11 @@ export const PORT = 8080;
 // config();
 export const environment = {
     development: {
-        // serverURL: `http://localhost:${PORT}/`,
+        serverURL: `http://localhost:${PORT}/`,
         dbString: `mongodb+srv://psihoz_ykt:xmcqmdmjxslaogf@cluster0.rp2fj.gcp.mongodb.net/?retryWrites=true&w=majority`
     },
-    // production: {
-    //     serverURL: `http://localhost:${PORT}/`,
-    //     dbString: `mongodb+srv://psihoz_ykt:xmcqmdmjxslaogf@cluster0.rp2fj.gcp.mongodb.net/?retryWrites=true&w=majority`
-    // }
+    production: {
+        serverURL: `http://localhost:${PORT}/`,
+        dbString: `mongodb+srv://psihoz_ykt:xmcqmdmjxslaogf@cluster0.rp2fj.gcp.mongodb.net/?retryWrites=true&w=majority`
+    }
 }
