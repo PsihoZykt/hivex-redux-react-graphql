7b94668327b48f3eaf94b1aa4b57c3d1ccd9ee4f
psihozykt <psihozykt@gmail.com>
2022-06-22 14:36:23 +0900

    project clean up, add apollo basic setup with example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {gql, useQuery} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+query{
+  getFriends{
+    id
+    firstName
+    lastName
+  }
+}
+`;
 
 function App() {
+  const {loading, error, data} = useQuery(EXCHANGE_RATES);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
+      <div onClick={() => console.log(data)}> Clicl me </div>
     </div>
   );
 }
