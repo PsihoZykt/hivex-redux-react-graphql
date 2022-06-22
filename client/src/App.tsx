7cd66f5be4c6561071c74834e4deb9753b870df8
psihozykt <psihozykt@gmail.com>
2022-06-22 21:41:26 +0900

    trying to resolve db connection problem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@ import React, {useEffect} from 'react';
 import {useFriendQuery} from "./generated/graphql";
 
 function App() {
-  const {data, loading} = useFriendQuery();
-  useEffect(() => {
-    if (!loading) {
-      console.log(loading, data)
-    }
-  })
+  // const {data, loading} = useFriendQuery();
+  // useEffect(() => {
+  //   if (!loading) {
+  //     console.log(loading, data)
+  //   }
+  // })
   return (
     <div className="App">
       Hello world
