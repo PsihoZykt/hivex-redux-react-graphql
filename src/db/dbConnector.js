90a3e01d1b4341c09f277b541a1dffb71d765552
psihozykt <psihozykt@gmail.com>
2022-06-27 22:04:32 +0900

    db connection refactoring

--- src/db/dbConnector.js
+++ src/db/dbConnector.js
@@ -1,25 +1,18 @@
-import {environment} from "../config/config.js";
-import {
-    currencySchema,
-    mentorSchema,
-    projectSchema,
-    proxySchema,
-    userSchema
-} from "./schema/schemas.js";
+import {currencySchema, mentorSchema, projectSchema, proxySchema, requestSchema, userSchema} from "./schema/schemas.js";
 import mongoose from "mongoose";
+import {environment} from "../config/config.js";
+
 const env = process.env.NODE_ENV || "development";
 
 /**
  * Mongoose Connection
  **/
-console.log(env)
 mongoose.connect(environment[env].dbString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-let db = mongoose.connection;
-db.on('error', () => {
+mongoose.connection.on('error', () => {
     console.error("Error while connecting to DB");
 });
 
@@ -29,5 +22,6 @@ const Projects = mongoose.model('Projects', projectSchema)
 const Mentors = mongoose.model('Mentors', mentorSchema)
 const Proxies = mongoose.model("Proxies", proxySchema)
 const Currencies = mongoose.model("Currency", currencySchema)
+const Requests = mongoose.model("Request", requestSchema)
 
-export  { Users, Projects, Mentors, Proxies, Currencies};
+export {Users, Projects, Mentors, Proxies, Currencies, Requests};
