ee0da169ace5a54899f7fff62465b20e782f6e27
psihozykt <psihozykt@gmail.com>
2022-06-27 22:11:40 +0900

    Remove redux, configure apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,13 @@
-import React, {useEffect} from 'react';
-// import {useFriendQuery} from "./generated/graphql";
-import {gql, useMutation} from "@apollo/client";
+import React from 'react';
+import ConsolePage from "components/ConsolePage/ConsolePage";
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
-  // const {data, loading} = useExampleQueryQuery();
-  // const [mutateFunction, {data, loading, error}] = useCurrencyMutation();
-  const mutation = gql`mutation {
-  addCurrency (
-    currencies: {
-      code: "dsa",
-      name: "das"
-    }
-  ){
-    code
-name
-  }
-}
-
-`
-  const [mutateFunction, {data, loading, error}] = useMutation(mutation);
-  useEffect(() => {
-    console.log(loading)
-
-
-    if (!loading) console.log(data)
-  }, [loading])
-
   return (
-    <div onClick={() => {
-      mutateFunction({variables: {currencies: {code: "1", name: "2"}}}).then(r => console.log("Mutation", data));
-
-    }} className="App">
-      Hello world
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<ConsolePage/>}/>
+      </Routes>
     </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
-import {Provider} from 'react-redux'
-import {store} from "./store/store";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const LOCALHOST_GRAPHQL_URI = 'http://localhost:8080/graphql'
+const HEROKU_GRAPHQL_URI =  "https://hivex-redux-graphql.herokuapp.com/graphql"
+const API_URI = process.env.NODE_ENV === "development" ? LOCALHOST_GRAPHQL_URI :  HEROKU_GRAPHQL_URI
 const client = new ApolloClient({
-  // uri: 'https://hivex-redux-graphql.herokuapp.com/graphql',
-  uri: 'http://localhost:8080/graphql',
+  uri: API_URI,
   cache: new InMemoryCache()
 
 });
 
 root.render(
-  <Provider store={store}>
-    <ApolloProvider client={client}>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
       <React.StrictMode>
         <App/>
       </React.StrictMode>
-    </ApolloProvider>
-  </Provider>
+    </BrowserRouter>
+  </ApolloProvider>
 );
